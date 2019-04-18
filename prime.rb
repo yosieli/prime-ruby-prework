4fbffcb1eac686b8be100da22e0813b247863bf5
feven <feventsegay6@gmail.com>
2019-04-18 03:33:22 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,13 @@
 # Add  code here!
-def prime?(num)
-   if num <= 1
-   false
-   
-  elsif(2..Math.sqrt(num)).none? { |i| (num % i).zero? }
-  true
-end
-end
+def prime?(nums)
+    nums.each do |num|
+    i = 2
+      while i < num
+        if num % i == 0
+          puts "#{num} is not a prime"
+        else
+          puts "#{num} is a prime"
+        end
+        i += 1
+      end
+   end
